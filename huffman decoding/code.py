35c6c995ad3cd9a58e4bf0a7d1bd16ac8c7bc785
burn3dx <bektemir.jalankuzov8899@gmail.com>
2026-04-16 00:01:08 +0500

    Update code.py

--- huffman decoding/code.py
+++ huffman decoding/code.py
@@ -1,1 +1,14 @@
+def huffman_decoding(encoded_text, huffman_codes):
+    # создаём обратный словарь
+    reverse_codes = {v: k for k, v in huffman_codes.items()}
 
+    current_code = ""
+    decoded_text = ""
+
+    for bit in encoded_text:
+        current_code += bit
+        if current_code in reverse_codes:
+            decoded_text += reverse_codes[current_code]
+            current_code = ""
+
+    return decoded_text
